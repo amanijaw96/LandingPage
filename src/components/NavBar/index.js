f768cb55a9064146f89906111be4d81c244b8661
amani <amanijaw1996>
2020-09-13 11:13:19 +0300

    navbar enhancement

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -10,22 +10,48 @@ import {
 import "./navbar.scss";
 
 export default function MyNavBar() {
+  const [expanded, setExpanded] = React.useState(false);
+  const [screenSize, setScreemSize] = React.useState(1200);
+
+  React.useEffect(() => {
+    setScreemSize(window.innerWidth);
+    window.addEventListener("resize", function () {
+      setScreemSize(window.innerWidth);
+    });
+  }, []);
+
   return (
-    <Navbar variant="dark" expand="lg">
+    <Navbar
+      variant="dark"
+      expand="lg"
+      bg={screenSize < 1200 && expanded ? "dark" : ""}
+    >
       <div className="container align-items-start d-flex">
         <Navbar.Brand href="/">
           <button className="pl-5 pr-5 pt-2 pb-2 Logo">Logo</button>
         </Navbar.Brand>
-        {/* <div className="d-flex flex-md-row flex-column-reverse"> */}
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+        {screenSize < 1200 ? (
+          <Navbar.Toggle
+            aria-controls="basic-navbar-nav"
+            onClick={() => setExpanded((curr) => !curr)}
+          />
+        ) : null}
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className={`justify-content-end ${expanded === true ? "d-flex" : ""}`}
+        >
           <Navbar.Text style={{ color: "#ffffff" }}>Login</Navbar.Text>
           <div className="userIcon mr-3">
             <i className="fa fa-user" style={{ color: "#ffffff" }}></i>
           </div>
         </Navbar.Collapse>
         <div className="break mr-3 d-none d-md-inline"></div>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        {/* </div> */}
+        {screenSize > 1200 ? (
+          <Navbar.Toggle
+            aria-controls="basic-navbar-nav"
+            onClick={() => setExpanded((curr) => !curr)}
+          />
+        ) : null}
       </div>
     </Navbar>
   );
